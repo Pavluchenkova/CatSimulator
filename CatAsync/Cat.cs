d4bfcdaa77d094f2010b49e5c3c5660a94180140
Agnessa Pavlyuchenkova <agnessa.pavlyuchenkova@gmail.com>
2017-02-22 13:54:37 +0200

    Add events

--- CatAsync/Cat.cs
+++ CatAsync/Cat.cs
@@ -10,43 +10,88 @@ namespace CatAsync
     class Cat
     {
         public string Name { get; private set; }
-        public uint _energy = 100;
+        public uint _energy;
+        private CatState _state;
+
+        public delegate void CatEventHandler();
+        public event CatEventHandler CatFed;
+        public event CatEventHandler CatHungy;
+
         public Cat(string name)
         {
-            Name = name;           
+            Name = name;
+            _energy = 100;
+            _state = CatState.Fed;
+            CatFed += SayPurpur;
+            CatHungy += SayMeow;
         }
+
         
         public void Feeding()
         {
             _energy += 10;
-            if (_energy >= 70)
+
+            if (_energy >= 70 && _state == CatState.Hungry)
             {
-                SayPurpur();
+                CatFed?.Invoke();
+                _state = CatState.Fed;
             }
+            if (_energy >= 100)
+            { 
+                _state = CatState.Satiated;
+            }
+
         }
 
+        public void Pet()
+        {
+            SayPurpur();
+        }
+
+        public void Call()
+        {
+            SayMeow();
+        }
+        public string CatImage()
+        {
+            return String.Format(@"
+ .       .
+ |\_---_/|
+/   o_o   \
+|    U    |
+\  ._I_.  /
+ `-_____-'");
+        }
+        
+        
         public void Physiology()
         {
             do
             {
                 var interval = 1000;
                 Thread.Sleep((int)interval);
-               
-                if(_energy <= 60)
+
+                if (_energy >= 70 && _energy < 100)
                 {
-                    SayMeow();
+                    _state = CatState.Fed;
+
+                }
+                if(_energy <= 60 && _state == CatState.Fed)
+                {
+                    CatHungy?.Invoke();
+                    _state = CatState.Hungry;
                 }
                 _energy--;
 
             } while (_energy != 0);
         }
 
-        public void SayPurpur ()
+        private void SayPurpur ()
         {
             Console.WriteLine("Pur-Pur...");
         }
 
-        public void SayMeow()
+        private void SayMeow()
         {
             Console.WriteLine($"Meow! ({this._energy})");
         }
--- CatAsync/Program.cs
+++ CatAsync/Program.cs
@@ -10,22 +10,37 @@ namespace CatAsync
     {
         static void Main(string[] args)
         {
-            Cat myCat = new Cat("Barsyk");
             
+            Console.WriteLine("Give me a name:");
+            string name = Console.ReadLine();
+
+            Cat myCat = new Cat(name);
 
             Task task = new Task(myCat.Physiology);
             task.Start();
-            Console.WriteLine($"Feed {myCat.Name}:");
-           
+            Console.WriteLine($"I'm your cat {myCat.Name} \nPlease press: \nF - feed \nC - call \nP - pet \nEsc - exit ");
+            Console.WriteLine($"{myCat.CatImage()}");
+   
             var working = true;
+
             while (working)
             {
                 var key = Console.ReadKey(true).Key;
-                if (key == ConsoleKey.F)
+                switch (key)
                 {
-                    myCat.Feeding();
+                    case ConsoleKey.F:
+                        myCat.Feeding();
+                        break;                 
+                    case ConsoleKey.C:
+                        myCat.Call();
+                        break;
+                    case ConsoleKey.P:
+                        myCat.Pet();
+                        break;
+                    case ConsoleKey.Escape:
+                            working = false;
+                        break;
                 }
-                if (key == ConsoleKey.Escape) working = false;
             }
         }
     }
